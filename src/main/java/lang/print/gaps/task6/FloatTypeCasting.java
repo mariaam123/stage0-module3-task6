1a1279cb001600d2dacb06ea8cc48d7d4d3a4632
mariaam123 <mariam.bakradze.1@btu.edu.ge>
2023-08-01 03:10:39 +0400

    edited

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        
+        int result = Math.round(numberToBeRounded);
+
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        float numberToBeRounded = 19.4f;
+        floatTypeCasting.roundNumber(numberToBeRounded);
     }
 }
